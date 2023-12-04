783c94f21b0a4a132e497aa114cdc4c0b7d6d767
Pavlov Danil <danpavkzn@mail.ru>
2023-12-05 01:37:03 +0300

    успешное удаление и добавление заказов 5.12.2023 01:36

--- 123/main.py
+++ 123/main.py
@@ -1,7 +1,2 @@
 # from config import *
 # from classes import *
-#
-# mycursor = mydb.cursor()
-# mycursor.execute("insert into zakazu (idzakazu, usluga, cost, company)"
-#                  " values (1, 'gaga', 15000, 'govnoed')")
-# mydb.commit()
