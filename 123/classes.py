de800928f5f5fc6da5bcda60bc47d60c919e2567
Pavlov Danil <danpavkzn@mail.ru>
2023-12-04 11:39:17 +0300

    парочку прик 2

--- 123/classes.py
+++ 123/classes.py
@@ -11,26 +11,35 @@ class Uslugi:
     # объявить здесь конструктор принимает имя компании и при этом наследуется вниз
     # еще может что-то добавить сюда
 
+    @staticmethod
+    def info():
+        print('заказ оформлен')
+
+    @staticmethod
+    def del_info():
+        print('Заказ удален')
+
 
 class Zakazu(Uslugi):
     def __init__(self, name):
         self.name = name  # услуга передается через наследование сюда
 
-    def add_zakaz(self, idz, cos, com):
+    def add_zakaz(self,cos, com):
         mycursor = mydb.cursor()
         usl = self.name
-        idz = int(idz)
         cos = int(cos)
-        mycursor.execute(f"insert into zakazu (idzakazu, usluga, cost, company)"
-                         f" values ({idz}, '{usl}', {cos}, '{com}')")
+        mycursor.execute(f"insert into zakazu (usluga, cost, company)"
+                         f" values ('{usl}', {cos}, '{com}')")
         mydb.commit()
+        self.info()
 
     def remove_zakaz(self, idz):
         mycursor = mydb.cursor()
         idz = int(idz)
         mycursor.execute("delete from zakazu where idzakazu = idz")
         mydb.commit()
+        self.del_info()
 
 
 s = Zakazu('atefirma')
-s.add_zakaz(1, 15000, 'hi_hitler')
+s.add_zakaz(15000, 'hi_hitler')
