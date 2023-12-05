c47c9c50d671022387da10baf4499319dfd20883
Pavlov Danil <danpavkzn@mail.ru>
2023-12-05 14:56:56 +0300

    14:56

--- 123/classes.py
+++ 123/classes.py
@@ -11,15 +11,15 @@ class Uslugi:
 
     @staticmethod
     def info():
-        print('заказ оформлен')
+        print('заказ оформлен\n')
 
     @staticmethod
     def del_info():
-        print('Заказ удален')
+        print('Заказ удален\n')
 
     @staticmethod
     def change_info():
-        print('Статус заказа изменен')
+        print('Статус заказа изменен\n')
 
 
 class Zakazu(Uslugi):
@@ -30,6 +30,7 @@ class Zakazu(Uslugi):
     def add_zakaz(cls):
         mycursor = mydb.cursor()
         com = input('Наименование компании : ')
+        cls.check(com)
         for k, v in cls.uslugi.items():
             print(k, v[0], sep=' : ', end='\n')
 
@@ -50,11 +51,12 @@ class Zakazu(Uslugi):
         for i in zakaz:
             print(f"id : {i[0]}, компания : {i[3]}, услуга - {i[1]}, по цене : {i[2]} руб.")
         n = int(input('Выберите id заказа, статус которого хотите поменять : '))
+        cls.check(n)
         if n not in [i[0] for i in zakaz]:
             print("id заказа не существует")
             pass
-        status1 = ['в обработке', 'в процессе', 'Выполнен'][int(input("1 : В обработке"
-                                                                      "\n2 : В процессе\n3 : Выполнен\n")) - 1]
+        status1 = ['в обработке', 'в процессе', 'Выполнен'][
+            int(input("1 : В обработке\n2 : В процессе\n3 : Выполнен\n")) - 1]
         mycursor.execute(f"update zakazu set status = '{status1}' where idzakazu like {n}")
         mydb.commit()
         cls.change_info()
@@ -63,6 +65,7 @@ class Zakazu(Uslugi):
     def remove_zakaz(cls):
         mycursor = mydb.cursor()
         n = input('Введите наименование компании : ')
+        cls.check(n)
         mycursor.execute("select company from zakazu")
         zakaz = mycursor.fetchall()
         if n not in zakaz:
@@ -71,8 +74,11 @@ class Zakazu(Uslugi):
         mycursor.execute(f"select * from zakazu where company like '{n}'")
         zakaz = mycursor.fetchall()
         for i in zakaz:
-            print(f"id : {i[0]}, компания : {i[3]}, услуга - {i[1]}, по цене : {i[2]} руб., статус заказа : {i[3]}")
-        n = int(input('Выберите id заказа который хотите удалить : '))
+            print(f"id : {i[0]}, компания : {i[3]}, услуга - {i[1]}, по цене : {i[2]} руб., статус заказа : {i[4]}")
+        n = int(input('Выберите id заказа который хотите отменить : '))
+        if n not in [i[0] for i in zakaz]:
+            print("id заказа не существует")
+            pass
         mycursor.execute(f"delete from zakazu where idzakazu = {n}")
         mydb.commit()
         cls.del_info()
@@ -83,22 +89,33 @@ class Zakazu(Uslugi):
         mycursor.execute(f"select * from zakazu")
         zakaz = mycursor.fetchall()
         for i in zakaz:
-            print(f"id : {i[0]}, компания : {i[3]}, услуга - {i[1]}, по цене : {i[2]} руб., статус заказа : {i[3]}")
+            print(f"id : {i[0]}, компания : {i[3]}, услуга - {i[1]}, по цене : {i[2]} руб., статус заказа : {i[4]}")
 
     @classmethod
     def show_status(cls):
-        n = int(input('Введите id заказа для информации о статусе доставки : '))
         mycursor = mydb.cursor()
+        n = int(input('Введите id заказа для информации о статусе доставки : '))
+        cls.check(n)
+        mycursor.execute(f"select idzakazu from zakazu")
+        zakaz = list(list(i)[0] for i in mycursor.fetchall())
+        if n not in zakaz:
+            print("id заказа не существует")
+            exit()
         mycursor.execute(f"select usluga, status from zakazu where idzakazu like '{n}'")
         zakaz = mycursor.fetchall()
         for i in zakaz:
             print(f"Услуга : {i[0]}, статус заказа : {i[1]}")
 
+    @classmethod
+    def check(cls, obj):
+        if obj == '':
+            print('пустое поле')
+            exit()
+
 
 s = Zakazu()
-s.add_zakaz()
-s.change_status()
-s.remove_zakaz()
+# s.add_zakaz()
+# s.change_status()
+# s.remove_zakaz()
 s.show_status()
-s.show_all()
-
+# s.show_all()
